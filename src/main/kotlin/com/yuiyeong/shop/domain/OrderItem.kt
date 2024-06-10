c866fcce78059b5763542140c5de64f1ad559cec
yuiyeong <joyuiyeong@gmail.com>
2024-06-10 15:06:46 +0900

    Refactor: OrderItem domain model

--- src/main/kotlin/com/yuiyeong/shop/domain/OrderItem.kt
+++ src/main/kotlin/com/yuiyeong/shop/domain/OrderItem.kt
@@ -4,26 +4,33 @@ import com.yuiyeong.shop.domain.item.Item
 import jakarta.persistence.*
 
 @Entity
-class OrderItem protected constructor(
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "item_id")
-    var item: Item,
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "order_id")
-    var order: Order?,
-
-    var oderPrice: Int,
-    var count: Int,
-
+class OrderItem(item: Item, orderPrice: Int, count: Int) {
     @Id
     @GeneratedValue
     @Column(name = "order_item_id")
     var id: Long? = null
-) {
+        private set
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    var item: Item = item
+        private set
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    var order: Order? = null
+        private set
+
+    var oderPrice: Int = orderPrice
+        private set
+
+    var count: Int = count
+        private set
+
+
     companion object {
         fun createOrderItem(item: Item, orderPrice: Int, count: Int): OrderItem {
-            val orderItem = OrderItem(item, null, orderPrice, count)
+            val orderItem = OrderItem(item, orderPrice, count)
             item.reduceStockQuantity(count)
             return orderItem
         }
