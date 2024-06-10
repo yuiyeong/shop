1921638ac1b90eedbc4b67d4f35feeb1271dce5b
yuiyeong <joyuiyeong@gmail.com>
2024-06-10 15:43:11 +0900

    Feat: Add more find methods into MemberRepository

--- src/main/kotlin/com/yuiyeong/shop/repository/MemberRepository.kt
+++ src/main/kotlin/com/yuiyeong/shop/repository/MemberRepository.kt
@@ -17,4 +17,14 @@ class MemberRepository(@Autowired val em: EntityManager) {
         return em.find(Member::class.java, id)
     }
 
+    fun findAll(): List<Member> {
+        return em.createQuery("select m from Member m", Member::class.java).resultList
+    }
+
+    fun findByName(name: String): List<Member> {
+        return em.createQuery(
+            "select m from Member m where m.name = :name",
+            Member::class.java
+        ).setParameter("name", name).resultList
+    }
 }
